7f7fae935f95eae8fd1e778bf2ef6bf9a8d33f3f
Carlos Teles <carlos.teles@eic.cefet-rj.br>
2020-01-08 21:44:46 -0300

    Projeto Vetor_11

---
+++ 11_Vetor/src/controller/ExemploVetorInt.java
@@ -0,0 +1,17 @@
+package controller;
+
+public class ExemploVetorInt {
+	public static void main(String[] args) {
+		int numeros[] = new int[4];
+		numeros[0] = 6;
+		numeros[1] = 1163;
+		numeros[2] = 17;
+		System.out.println("numeros[2] = " + numeros[2]);
+		//numeros[3] = -979879798;
+		numeros[2] = -1; 
+		
+		System.out.println("numeros[2] = " + numeros[2]);
+		
+		System.out.println("numeros[3] = " + numeros[3]);
+	}
+}
