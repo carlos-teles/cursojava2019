f874f936963d33773e9ee312f6cc08d049d03339
Carlos Teles <carlos.teles@eic.cefet-rj.br>
2020-01-10 21:44:04 -0300

    Vetor_11

---
+++ 11_Vetor/src/controller/ExemploVetorFloat.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class ExemploVetorFloat {
+
+	public static void main(String[] args) {
+		 float notas[] = { 4, 7.5f, 10, 9.55555554f };
+		 System.out.println("notas[0] = " + notas[0]);
+		 System.out.println("notas[1] = " + notas[1]);
+		 System.out.println("notas[2] = " + notas[2]);
+		 System.out.println("notas[3] = " + notas[3]);
+		 }
+
+}
